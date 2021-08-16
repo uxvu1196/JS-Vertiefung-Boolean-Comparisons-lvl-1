b4c1343e0246b1e9936b3285f41aba47e89cefbd
uxvu1196 <uxvu1196@gmail.com>
2021-08-16 15:53:20 +0200

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+console.log("hello Boolean");
+document.write("Übung lev1_14");
+
+console.log(Boolean(100))
+console.log(Boolean(3.14))
+console.log(Boolean(-15))
+console.log(Boolean("hello"))
+console.log(Boolean("false"))
+console.log(Boolean(1 + 7 + 3.14))
+console.log(Boolean(0))
+console.log(Boolean(-0))
+console.log(Boolean(NaN))
+console.log(Boolean(null))
+console.log(Boolean(false))
+
+
+document.write("Übung lev1_15");
+
+let num = 5;
+let anotherNum = 8;
+console.log(num == anotherNum)
+console.log(num > anotherNum)
+console.log(num < anotherNum)
+console.log(num >= anotherNum)
+console.log(num <= anotherNum)
+
+console.log(num == "5")
+console.log(num != 8)
+console.log(num != "5")
+
+
+
